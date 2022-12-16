62aa737ed3d22789f43008387d2f450b463d1913
Dylan Praul <git@dylanpraul.com>
2022-12-16 13:45:47 -0500

    ci: add nightly builds
    Updates the CI pipeline and helpers to enable nightly builds.

--- helpers/get-image-tags.sh
+++ helpers/get-image-tags.sh
@@ -4,16 +4,19 @@ set -e
 # Get the set of tags to release the given image
 #
 # Usage:
-# ./get-release-sha.sh <IMAGE_NAME> <PHP_VERSION> <DISTRO> <COMMITHASH>
+# ./get-release-sha.sh <IMAGE_NAME> <PHP_VERSION> <DISTRO> <COMMITHASH> [BUILD_PREFIX]
 #
 # Example:
-# ./get-release-sha.sh fintechstudios/davical 8.1.3 bullseye b62fa33a
+# ./get-release-sha.sh fintechstudios/davical 8.1.3 bullseye b62fa33a latest
 ###
+
+MAIN_DISTRO="bullseye"
 
 IMAGE_NAME=$1
 PHP_VERSION=$2
 DISTRO=$3
 COMMITHASH=$4
+BUILD_PREFIX=$5
 
 beginswith() { case $2 in "$1"*) true;; *) false;; esac; }
 
@@ -32,10 +35,23 @@ davical_version=$(get_label "com.fts.davical-version")
 awl_version=$(get_label "com.fts.awl-version")
 short_hash=$(echo "$COMMITHASH" | cut -c 1-8)
 
+if [ "$BUILD_PREFIX" = "latest" ]; then
+  distro_version="$DISTRO"
+  main_version="latest"
+else
+  distro_version="$DISTRO-nightly"
+  main_version="nightly"
+fi
+
+if [ $DISTRO != $MAIN_DISTRO ]; then
+  main_version=""
+fi
+
 echo "\
 $davical_version-awl$awl_version-php$PHP_VERSION-$DISTRO-$short_hash \
 $davical_version-awl$awl_version-php$PHP_VERSION-$DISTRO \
 $davical_version-php$PHP_VERSION-$DISTRO \
 $davical_version-$DISTRO \
 $davical_version \
-$DISTRO"
+$distro_version \
+$main_version"
