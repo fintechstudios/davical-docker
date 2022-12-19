4f9ba88a94cfff3d24ecdd92e70f3e52d22ab6d4
Dylan Praul <dpraul@users.noreply.github.com>
2022-12-19 09:43:20 -0500

    ci: add nightly builds (#8)
    Updates the CI pipeline and helpers to enable nightly builds.

--- helpers/get-release-sha.sh
+++ helpers/get-release-sha.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 ###
-# Get the SHA512 for the tarball of the specified davical and awl versions.
+# Get the SHA512 for the tarball of the specified davical and awl versions. Creates .versions.env
 #
 # Usage:
 # ./get-release-sha.sh <DAVICAL_VERSION> <AWL_VERSION>
+# ./source .versions.env
 #
 # Should work with tag names, branch names, or commit hashes as the version name, e.g.
 # ./get-release-sha.sh r1.1.10 r0.62
@@ -13,11 +14,43 @@ set -e
 DAVICAL_VERSION="$1"
 AWL_VERSION="$2"
 
-DAVICAL_URL=https://gitlab.com/davical-project/davical/-/archive/${DAVICAL_VERSION}/davical.tar.gz
-AWL_URL=https://gitlab.com/davical-project/awl/-/archive/${AWL_VERSION}/awl.tar.gz
+if [ -z $DAVICAL_VERSION ] || [  -z $AWL_VERSION ]; then
+  echo "Must specify davical and awl version"
+  exit 1
+fi
 
-echo "DAViCal $DAVICAL_VERSION sha512sum:"
-curl -s $DAVICAL_URL | sha512sum -
+DAVICAL_URL="https://gitlab.com/davical-project/davical/-/archive/${DAVICAL_VERSION}/davical.tar.gz"
+AWL_URL="https://gitlab.com/davical-project/awl/-/archive/${AWL_VERSION}/awl.tar.gz"
+ENV_FILENAME=".versions.env"
 
-echo "AWL $AWL_VERSION sha512sum:"
-curl -s $AWL_URL | sha512sum -
+get_commit_hash() {
+  echo "$1" | sed -nr 's/[^-]*-[^-]*-([a-z0-9]*)\.tar\.gz/\1/p'
+}
+
+temp_path="./.tmp-$(date +"%s")"
+
+mkdir "$temp_path"
+cd "$temp_path"
+
+curl -JOSs "$DAVICAL_URL"
+davical_filename=$(find . -name "davical-*.tar.gz")
+DAVICAL_COMMITHASH=$(get_commit_hash "$davical_filename")
+DAVICAL_SHA512=$(sha512sum "$davical_filename" | cut -d " " -f 1)
+
+curl -JOSs "$AWL_URL"
+awl_filename=$(find . -name "awl-*.tar.gz")
+AWL_COMMITHASH=$(get_commit_hash "$awl_filename")
+AWL_SHA512=$(sha512sum "$awl_filename" | cut -d " " -f 1)
+
+cd ..
+rm -r "$temp_path"
+
+echo "\
+DAVICAL_VERSION=\"$DAVICAL_COMMITHASH\"
+DAVICAL_SHA512=\"$DAVICAL_SHA512\"
+AWL_VERSION=\"$AWL_COMMITHASH\"
+AWL_SHA512=\"$AWL_SHA512\"
+DOCKER_BUILD_FLAGS=\"--build-arg DAVICAL_VERSION --build-arg DAVICAL_SHA512 --build-arg AWL_VERSION --build-arg AWL_SHA512\"
+BUILD_PREFIX=\"$DAVICAL_COMMITHASH-awl$AWL_COMMITHASH\"
+" > "$ENV_FILENAME"
+echo "Created $ENV_FILENAME"
